27a90fa227d3fe635301d17b0f5e5a0a5278ce5f
Timothy Small <smalls.online@icloud.com>
2021-10-14 10:33:03 -0400

    Updated build script
    - Added verbose messages.
    - Added framework version selection for building.
     - '-FrameworkVersion' accepts multiple versions.
     - '-BuildAllFrameworkVersions' builds all possible versions.
    - Changed the verbosity of 'dotnet' processes.
    - Added 'Push-Location' and 'Pop-Location' to the build to ensure that
      the script's execution is done in the right directory.

--- buildLib.ps1
+++ buildLib.ps1
@@ -1,10 +1,52 @@
 [CmdletBinding()]
-param()
+param(
+    [Parameter(Position = 0, ParameterSetName = "SpecificVersions")]
+    [ValidateSet(
+        "net5.0",
+        "netstandard2.1",
+        "netframework4.5"
+    )]
+    [string[]]$FrameworkVersion = "net5.0",
+    [Parameter(Position = 0, ParameterSetName = "AllVersions")]
+    [switch]$BuildAllFrameworkVersions
+)
 
-# Builds the library.
+$frameworkVersions = $null
+switch ($PSCmdlet.ParameterSetName) {
+    "AllVersions" {
+        $frameworkVersions = @(
+            "net5.0",
+            "netstandard2.1",
+            "netframework4.5"
+        )
+        break
+    }
 
-dotnet restore
-dotnet clean
-dotnet publish --framework net5.0
-dotnet publish --framework netstandard2.1
-dotnet publish --framework netframework4.5
+    Default {
+        $frameworkVersions = $FrameworkVersion
+        break
+    }
+}
+
+Write-Verbose "The following frameworks will be targetted for building: $($frameworkVersions -join ', ')"
+
+$scriptRoot = $PSScriptRoot
+
+Push-Location -Path $scriptRoot
+
+try {
+    # Builds the library.
+    Write-Verbose "Running 'dotnet restore'."
+    dotnet restore --nologo --verbosity quiet
+
+    Write-Verbose "Running 'dotnet clean'."
+    dotnet clean --nologo --verbosity quiet
+
+    foreach ($frameworkVersionItem in $frameworkVersions) {
+        Write-Verbose "Running 'dotnet publish --framework $($frameworkVersionItem)'."
+        dotnet publish --nologo --verbosity minimal --framework $frameworkVersionItem
+    }
+}
+finally {
+    Pop-Location
+}
