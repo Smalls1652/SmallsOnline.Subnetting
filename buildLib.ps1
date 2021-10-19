8cefd76b7ea45bc92369e1bdb6b5538825ad6864
Timothy Small <smalls.online@icloud.com>
2021-10-19 16:40:06 -0400

    Updated class library build script
    - The class library build script will now point directly to the
      dedicated solution file for it.

--- buildLib.ps1
+++ buildLib.ps1
@@ -29,20 +29,21 @@ switch ($PSCmdlet.ParameterSetName) {
 Write-Verbose "The following frameworks will be targetted for building: $($frameworkVersions -join ', ')"
 
 $scriptRoot = $PSScriptRoot
+$libSlnPath = Join-Path -Path $scriptRoot -ChildPath "SmallsOnline.Subnetting.sln"
 
 Push-Location -Path $scriptRoot
 
 try {
     # Builds the library.
     Write-Verbose "Running 'dotnet restore'."
-    dotnet restore --nologo --verbosity quiet
+    dotnet restore "$($libSlnPath)" --nologo --verbosity quiet
 
     Write-Verbose "Running 'dotnet clean'."
-    dotnet clean --nologo --verbosity quiet
+    dotnet clean "$($libSlnPath)" --nologo --verbosity quiet
 
     foreach ($frameworkVersionItem in $frameworkVersions) {
         Write-Verbose "Running 'dotnet publish --framework $($frameworkVersionItem)'."
-        dotnet publish --nologo --verbosity minimal --framework $frameworkVersionItem
+        dotnet publish "$($libSlnPath)" --nologo --verbosity minimal --framework $frameworkVersionItem
     }
 }
 finally {
